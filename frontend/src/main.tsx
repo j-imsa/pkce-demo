19c9be188bd0cb7c51c304cdebb2e47e4059e7c4
Iman Salehi <cse.isalehi@gmail.com>
2025-09-26 19:02:35 +0330

    add oidc config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,21 @@
 import {createRoot} from 'react-dom/client'
 import {AppRouter} from "./routing/AppRouter.tsx";
+import {AuthProvider} from "react-oidc-context";
 import './index.css'
 
+const oidcConfig = {
+    authority: "http://localhost:8080/realms/pkce-demo",
+    client_id: "my-react-app",
+    redirect_uri: window.location.origin + "/dashboard",
+    post_logout_redirect_uri: window.location.origin + "/logout",
+    response_type: "code",
+    code_challenge_method: "S256",
+    scope: "openid profile email offline_access",
+    automaticSilentRenew: true,
+};
+
 createRoot(document.getElementById('root')!).render(
-    <AppRouter/>
+        <AuthProvider {...oidcConfig}>
+            <AppRouter/>
+        </AuthProvider>
 )
