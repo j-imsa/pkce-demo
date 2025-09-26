f6f2305e1da5bf4af63955fb59591de5158b4329
Iman Salehi <cse.isalehi@gmail.com>
2025-09-26 19:05:08 +0330

    update dashboard page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,9 +1,33 @@
+import {useAuth} from "react-oidc-context";
+import {useState} from "react";
+import api from "../api/Api.tsx";
+
 export const DashboardPage = () => {
+    const auth = useAuth();
+    const [list, setList] = useState<string[]>([]);
 
-
+    const fillList = async () => {
+        try {
+            const res = await api.get<string[]>("/list");
+            setList(res.data);
+        } catch (err) {
+            console.error("API call failed", err);
+        }
+    }
     return (
         <div>
-            Dashboard Page
+            <h1>Dashboard (Protected)</h1>
+            <p>Welcome {auth.user?.profile?.preferred_username ?? auth.user?.profile?.email}</p>
+            <button onClick={() => void auth.removeUser()}>Log out (local)</button>
+            <button onClick={() => void auth.signoutRedirect()}>Log out (global)</button>
+            <div>
+                <button onClick={fillList}>Call API</button>
+                <div>
+                    {list.map(item => (
+                        <p key={item}>{item}</p>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
