a42ddacb60df076fefe81e44c387c9127461022b
Iman Salehi <cse.isalehi@gmail.com>
2025-09-28 22:23:55 +0330

    add keycloakify module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {keycloakify} from "keycloakify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        keycloakify({
+            themeName: "NewTheme",
+            accountThemeImplementation: "none"
+        })
+    ],
     server: {
         port: 5678
     }
