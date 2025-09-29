d59cffe92e3daadba391e9b66b5202f6473acaad
Iman Salehi <cse.isalehi@gmail.com>
2025-09-29 09:54:05 +0330

    install tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import {keycloakify} from "keycloakify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        tailwindcss(),
         keycloakify({
             themeName: "NewTheme",
             accountThemeImplementation: "none"
