762ec00fc601c40c386a4df8472c34ecd433df31
Iman Salehi <cse.isalehi@gmail.com>
2025-09-30 13:23:46 +0330

    add get all users

--- backend/src/main/java/com/pkcedemo/backend/controller/UserController.java
+++ backend/src/main/java/com/pkcedemo/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.pkcedemo.backend.controller;
 
 import com.pkcedemo.backend.model.dto.UserRegistrationDto;
 import com.pkcedemo.backend.model.entity.User;
+import com.pkcedemo.backend.repository.UserRepository;
 import com.pkcedemo.backend.service.UserManagementService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,17 +11,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserManagementService userManagementService;
+    private final UserRepository userRepository;
 
     @PostMapping("/register")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<User> registerUser(@Valid @RequestBody UserRegistrationDto registrationDto) {
         User user = userManagementService.registerUser(registrationDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll(); // App User
+        return ResponseEntity.ok(users);
     }
 
     @DeleteMapping("/{userId}")
